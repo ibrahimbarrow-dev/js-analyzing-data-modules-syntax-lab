e04d0caa90114cabdb088ed7a69ce06291e98913
ibrahimbarrow-dev <ibrahim.barrow@student.moringaschool.com>
2026-07-07 23:18:45 +0300

    feat: declare combineUsers function with rest parameters and baseline object

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 require('datejs');
 
+function combineUsers(...args) {
+  // Define the variable combinedObject
+  const combinedObject = {
+    users: []
+  };
+
+  return combinedObject;
+}
+
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
